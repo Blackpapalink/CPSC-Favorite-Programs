a352c0031c4eac9a827d5ccd0fa050573c7ffbfe
Blackpapalink <bluepapalink@yahoo.com>
2016-12-01 11:24:57 -0600

    Escape Sequence

---
+++ Excercise 3.py
@@ -0,0 +1,5 @@
+#Print What's What's going on?Dinosaurs say 'rawr!'/ is a slash, and \\ is a pair ofbackslashes\o/ means yay!
+print('What\'s going on?\nDinosaurs say \'rawr\' \n/ is a slash, and \\ \\ is a pair of back slashes \n\\o/ means yay!')
+#Print these
+fourlineswere printed by one print statement
+print('these \nfour \nlines \nwere printed by one print statement') 
